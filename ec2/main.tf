71332d03bc97778b95ff79bc28ae5c6be20019d5
iali2 <imran.ali@appdynamics.com>
2021-05-03 16:52:35 +0530

    addding ec2

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,15 +1,15 @@
 terraform {
   backend "s3" {}
-  required_version = "0.11.14"
+  required_version = ">= 0.12.17"
 }
 module "ec2" {
   source = "git::https://github.com/emraanali11/terraform_modules.git"
-  instance_type = "${var.instance}"
+  instance_type = "var.instance"
 }
 
 module "ec21" {
   source = "git::https://github.com/emraanali11/terraform_modules.git"
-  instance_type = "${var.instance1}"
+  instance_type = "var.instance1"
 }
 
 
