ab12c38b43e0739d5d6c00cd20bfc323751e28ce
iali2 <imran.ali@appdynamics.com>
2021-05-05 14:46:36 +0530

    adding new struct

--- infrastructure/provisioning/services/iam.tf
+++ infrastructure/provisioning/services/iam.tf
@@ -1,14 +1,4 @@
-terraform {
-  backend "s3" {}
-  required_version = ">= 0.12.17"
-
-}
-
-provider "aws" {
-  region = "${var.region}"
-  }
-
 module "iam" {
-  source        = "var.source"
+  source       = "var.source"
   cluster_name = var.cluster_name
 }
--- infrastructure/provisioning/services/vars.tf
+++ infrastructure/provisioning/services/vars.tf
@@ -1,22 +1,18 @@
 variable "cluster_name" {
-  type        = string
+  type    = string
   default = "test"
 }
 
-variable "region" { 
-	default = "us-west-2"
+variable "region" {
+  default = "us-west-2"
 }
 
 variable "source" {
-	default = git::https://github.com/emraanali11/terraform_modules.git//iam
+  default = "git::https://github.com/emraanali11/terraform_modules.git//iam"
 }
 
 variable "instance_type" {
   description = "The ec2 instance size you wish to create"
-  default = "t2.micro"
+  default     = "t2.micro"
   type        = string
-}
-
-variable "region" { 
-	default = "us-west-2"
 }
